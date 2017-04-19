5cc3e03c7ab2a93f50252935b17d126a5d32b20a
AnAverageGuy <jamschool@yahoo.com>
2017-04-18 22:07:34 -0500

    Framework for checkout pages

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Summary(models.Model):
+    rname = ""
+    saddress = ""
+    baddress = ""
+    stime = ""
+    ptype = ""
+    cardname = ""
+    cardnum = ""
+
+    def setrname(self, name):
+        self.rname = name
